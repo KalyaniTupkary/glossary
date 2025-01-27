c2af32e85c119652c9a9e374d69dba3eb25cd411
Jon Packles <jonpackles@MacBook-Pro.local>
2025-01-26 22:47:59 -0500

    increased tap area on edges mobile

--- js/calculatePosition.js
+++ js/calculatePosition.js
@@ -2,11 +2,11 @@ export function calculatePosition(index, currentIndex, isMobile, padding, elemen
     if (currentIndex === null) return "0px";
 
     if (index === currentIndex) {
-        return isMobile ? "12px" : `${padding * index}px`;
+        return isMobile ? "24px" : `${padding * index}px`;
     } else if (index > currentIndex) {
-        return isMobile ? `${24}px` : `${padding * index}px`;
+        return isMobile ? `${48}px` : `${padding * index}px`;
     } else if (index === currentIndex - 1) {
-        return isMobile ? `${-elementWidth + 12 + 2}px` : `-${elementWidth - padding * (index + 1) - 2}px`;
+        return isMobile ? `${-elementWidth + 24 + 2}px` : `-${elementWidth - padding * (index + 1) - 2}px`;
         // return isMobile ? `-${12}px` : `-${elementWidth - padding * (index + 1) - 1}px`;
     } else {
         return isMobile ? `${-elementWidth}px` : `-${elementWidth - padding * (index + 1) - 2}px`;
